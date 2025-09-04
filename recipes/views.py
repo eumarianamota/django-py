b76c64ce02374bf906e48b2bec4b8f6e0df37906
mariana <eumarianamota@gmail.com>
2025-09-04 17:09:27 -0300

    recipes website html

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
-
-
-def sobre(request):
-    return HttpResponse("SOBRE")
-
-
-def contato(request):
-    return HttpResponse("CONTATO")
-
-# Create your views here.
+    return render(request, 'recipes/pages/home.html')
