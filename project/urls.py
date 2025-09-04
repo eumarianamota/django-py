ec482f8e8b6b7b99e8e6cc4fd7d77d582e363273
mariana <eumarianamota@gmail.com>
2025-09-04 12:04:15 -0300

    views and templates

--- project/urls.py
+++ project/urls.py
@@ -1,26 +1,8 @@
-
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-
-# HTTP Request
-def home(request):
-    return HttpResponse("HOME")
-
-
-def sobre(request):
-    return HttpResponse("SOBRE")
-
-
-def contato(request):
-    return HttpResponse("CONTATO")
+from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('sobre/', sobre),
-    path('contato/', contato),
-
+    path('', include('recipes.urls'))
 ]
