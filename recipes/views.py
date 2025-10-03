7992d9eeebd1e441a00680a292870a9a12cfdd54
mariana <eumarianamota@gmail.com>
2025-10-03 16:07:52 -0300

    Fix: clean up page not found logic in category

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from .models import Recipe
-from django.http import Http404
 
 
 def home(request):
@@ -13,17 +12,16 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id,
-        is_published=True
-        ).order_by("-id")
-
-    if not recipes:
-        raise Http404('Page not found')
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True,
+        ).order_by('-id')
+    )
 
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
-        'title': f'{recipes.first().category.name} - category | '
+        'title': f'{recipes[0].category.name} - category | '
     })
 
 
